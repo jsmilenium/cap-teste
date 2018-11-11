42148794763206d70be6a06f94c88e54c321b87d
jsmilenium <jsmilenium2003@hotmail.com>
2018-11-11 16:10:21 -0300

    Update project

--- client/app/application/route.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module('app')
-  .config(['$routeProvider', function ($routeProvider) {
-      $routeProvider
-          .when("/", {templateUrl: "app/course/course.html", controller: "CourseCtrl"})
-          .when("/courseDetail", {templateUrl: "app/course/courseDetail.html", controller: "CourseDetailCtrl"})
-          .otherwise("/");
-  }]);
-  
